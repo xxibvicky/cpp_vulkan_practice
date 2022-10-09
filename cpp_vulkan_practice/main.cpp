d04445478cf6988c78693d7844106f1cec44faa0
xxibvicky <victoria.vock@s.hakmistelbach.ac.at>
2022-10-10 00:28:35 +0200

    created vulkan instance successfully

--- cpp_vulkan_practice/main.cpp
+++ cpp_vulkan_practice/main.cpp
@@ -5,6 +5,9 @@
 #include <stdexcept>
 #include <cstdlib>
 
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
 class HelloTriangleApplication {
 public:
     void run() {
@@ -15,24 +18,61 @@ public:
     }
 
 private:
+    GLFWwindow* window;
+    VkInstance instance;
+
     void initWindow() {
         glfwInit();
+
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
         glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-        GLFWwindow* window;
-        window = glfwCreateWindow(800, 600, "Vulkan", nullptr, nullptr);
+
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr); // width, height, title, monitor to open the window on (optional), (only relevant to OpenGL)
     }
 
     void initVulkan() {
-
+        createInstance();
     }
 
     void mainLoop() {
-
+        while (!glfwWindowShouldClose(window)) { // to keep the application running until either an error occurs or the window is closed
+            glfwPollEvents();
+        }
     }
 
-    void cleanup() {
+    void cleanup() { // cleaning up ressources once the window is closed
+        glfwDestroyWindow(window);
 
+        glfwTerminate();
+    }
+
+    void createInstance() {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+
+        VkResult result = vkCreateInstance(&createInfo, nullptr, &instance); // pointer to struct with creation info, pointer to custom allocator callbacks, pointer to the new project variable
+
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) { // check if the instance was created succesfully
+            throw std::runtime_error("Failed to create instance!");
+        }
     }
 };
 
